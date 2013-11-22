dddbdb5b277bdc56c449e24de49678c72b454c6a
stelter <stelter@tzi.de>
2013-11-22 17:30:39 +0100

    bisschen heiler

--- suturo_manipulation_service/src/suturo_manipulation_service_node.cpp
+++ suturo_manipulation_service/src/suturo_manipulation_service_node.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include <stdio.h>
+#include "suturo_manipulation_msgs/suturo_manipulation_srv.h"
 #include <moveit/move_group_interface/move_group.h>
 
 using namespace std;
+using namespace suturo_manipulation_msgs;
 
 int kinectToOdom(double &x, double &y, double &z)
 {
@@ -10,17 +12,17 @@ int kinectToOdom(double &x, double &y, double &z)
 	return 0;
 }
 
-/*bool moveb(suturo_manipulation_service::suturo_manipulation_srv::Request  &req,
-         suturo_manipulation_service::suturo_manipulation_srv::Response &res)
+bool moveb(suturo_manipulation_srv::Request  &req,
+         suturo_manipulation_srv::Response &res)
 {
     ros::AsyncSpinner spinner(1);
 	spinner.start();
 	
-	kinectToOdom(req.x, req.y, req.z);
+	//kinectToOdom(req.x, req.y, req.z);
 	
 	move_group_interface::MoveGroup group(req.arm);
 	
-	if (req.x == 0 && req.y == 0 && req.z == 0){
+	/*if (req.x == 0 && req.y == 0 && req.z == 0){
 		group.setNamedTarget(req.arm+"_home");
 	} else {
 		
@@ -29,26 +31,26 @@ int kinectToOdom(double &x, double &y, double &z)
 		cout << group.getCurrentPose().pose.position.z << endl;
 		
 		group.setPositionTarget(req.x, req.y, req.z);	
-	}
+	}*/
 
 	
 	res.succ = group.move();
 	if (res.succ){
-		ROS_INFO("%s moved to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
+		//ROS_INFO("%s moved to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
 	} else {
-		ROS_INFO("Error while moving %s to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
+		//ROS_INFO("Error while moving %s to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
 	}
 	return res.succ != 0;
-}*/
+}
 
 int main(int argc, char **argv)
 {
- /* ros::init(argc, argv, "t_move", ros::init_options::AnonymousName);
+  ros::init(argc, argv, "suturo_manipulation_srv", ros::init_options::AnonymousName);
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("t_move_srv", moveb);
+  ros::ServiceServer service = n.advertiseService("suturo_manipulation_srv", moveb);
   ROS_INFO("Ready to moveit!.");
   ros::spin();
-*/
+
   return 0;
 }
--- suturo_manipulation_service/src/test_move_client.cpp
+++ suturo_manipulation_service/src/test_move_client.cpp
@@ -1,9 +1,13 @@
 #include "ros/ros.h"
+#include "suturo_manipulation_msgs/suturo_manipulation_srv.h"
 #include <cstdlib>
+
+using namespace suturo_manipulation_msgs;
+using namespace std;
 
 int main(int argc, char **argv)
 {
-  /*ros::init(argc, argv, "t_move_client");
+  ros::init(argc, argv, "t_move_client");
   if (argc != 5)
   {
     ROS_INFO("usage: t_move X Y Z");
@@ -11,12 +15,12 @@ int main(int argc, char **argv)
   }
 
   ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<suturo_manipulation_service::suturo_manipulation_srv>("t_move_srv");
-  suturo_manipulation_service::suturo_manipulation_srv srv;
-  srv.request.arm = argv[1];  
+  ros::ServiceClient client = n.serviceClient<suturo_manipulation_srv>("t_move_srv");
+  suturo_manipulation_srv srv;
+  /*srv.request.arm = argv[1];  
   srv.request.x = atof(argv[2]);
   srv.request.y = atof(argv[3]);
-  srv.request.z = atof(argv[4]);
+  srv.request.z = atof(argv[4]);*/
   if (client.call(srv))
   {
     ROS_INFO("Sum: %ld", (long int)srv.response.succ);
@@ -25,7 +29,7 @@ int main(int argc, char **argv)
   {
     ROS_ERROR("Failed to call service t_move");
     return 1;
-  }*/
+  }
 
   return 0;
 }
