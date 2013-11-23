789c426bcefdbf3f15d92b364c5937e6d981f569
thocar <thocar@tzi.de>
2013-11-23 17:19:15 +0100

    added home_action_server

--- suturo_manipulation_action/src/suturo_manipulation_move_action_server.cpp
+++ suturo_manipulation_action/src/suturo_manipulation_move_action_server.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <suturo_manipulation_msgs/suturo_manipulation_moveAction.h>
+#include <suturo_manipulation_msgs/suturo_manipulation_homeAction.h>
 #include <moveit/move_group_interface/move_group.h>
 #include <suturo_manipulation_msgs/ActionAnswer.h>
 
@@ -48,12 +49,41 @@ void execute(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPt
 
 }
 
+void home(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPtr& goal, Server* as)
+{	
+	suturo_manipulation_msgs::suturo_manipulation_moveResult r;	
+	string arm = goal->arm;
+	
+	move_group_interface::MoveGroup group(arm);
+	
+	r.succ.header.stamp;
+	r.succ.type = suturo_manipulation_msgs::ActionAnswer::UNDEFINED;
+	
+	ROS_INFO("link: %s", group.getEndEffectorLink().c_str());
+	group.setNamedTarget(arm+"_home");
+	ROS_INFO("current pos: x=%f, y=%f, z=%f", group.getCurrentPose().pose.position.x,
+			group.getCurrentPose().pose.position.y,
+			group.getCurrentPose().pose.position.z);
+	
+	if (group.move()){
+	    r.succ.type = suturo_manipulation_msgs::ActionAnswer::SUCCESS;
+	    as->setSucceeded(r);
+	} else {
+		r.succ.type = suturo_manipulation_msgs::ActionAnswer::FAIL;
+		as->setAborted(r);
+	}	
+	ROS_INFO("moved: %i", r.succ.type);
+}
+
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "suturo_manipulation_move_server");
   ros::NodeHandle n;
   Server server(n, "move_action_server", boost::bind(&execute, _1, &server), false);
+  Server server_home(n, "home_action_server", boost::bind(&home, _1, &server), false);
   server.start();
+  server_home.start();
   ROS_INFO("Ready to moveit!.");
   ros::spin();
   return 0;
