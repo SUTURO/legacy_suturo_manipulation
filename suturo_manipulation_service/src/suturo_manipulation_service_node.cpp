6006385609d161a1438a75ad875aea8255694c5e
thocar <thocar@tzi.de>
2013-11-23 12:46:27 +0100

    added Answer Message

--- suturo_manipulation_service/src/suturo_manipulation_service_node.cpp
+++ suturo_manipulation_service/src/suturo_manipulation_service_node.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "suturo_manipulation_msgs/suturo_manipulation_srv.h"
 #include <moveit/move_group_interface/move_group.h>
+#include <suturo_manipulation_msgs/ActionAnswer.h>
 
 using namespace std;
 using namespace suturo_manipulation_msgs;
@@ -17,7 +18,10 @@ bool moveb(suturo_manipulation_srv::Request  &req,
 {
     ros::AsyncSpinner spinner(1);
 	spinner.start();
-	
+
+	suturo_manipulation_msgs::ActionAnswer answ;
+	answ.header.stamp;
+	answ.type = suturo_manipulation_msgs::ActionAnswer::UNDEFINED;
 	//kinectToOdom(req.x, req.y, req.z);
 	
 	move_group_interface::MoveGroup group(req.arm);
@@ -34,12 +38,16 @@ bool moveb(suturo_manipulation_srv::Request  &req,
 	}*/
 
 	
-	res.succ = group.move();
-	if (res.succ){
+	if (group.move()){
+	        answ.type = suturo_manipulation_msgs::ActionAnswer::SUCCES;
 		//ROS_INFO("%s moved to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
 	} else {
+                answ.type = suturo_manipulation_msgs::ActionAnswer::FAIL;
 		//ROS_INFO("Error while moving %s to x=%f y=%f z=%f", req.arm.c_str(), req.x, req.y, req.z);
 	}
+	// 1 geklappt, 0 nicht
+	res.succ=answ.type;
+	ROS_INFO("sending back response: [%ld]", res.succ);
 	return res.succ != 0;
 }
 
