156aa381cd256d15b1dac44c8b0132f1615e95f6
stelter <stelter@tzi.de>
2013-11-24 12:12:03 +0100

    refactor code & fixed bugs

--- suturo_manipulation_action/src/suturo_manipulation_move_action_server.cpp
+++ suturo_manipulation_action/src/suturo_manipulation_move_action_server.cpp
@@ -11,56 +11,61 @@ using namespace std;
 typedef actionlib::SimpleActionServer<suturo_manipulation_msgs::suturo_manipulation_moveAction> Server;
 
 tf::TransformListener* listener = NULL;
-geometry_msgs::PoseStamped kinectPose;
-geometry_msgs::PoseStamped odomPose;
 
-int kinectToOdom(double &x, double &y, double &z, const char* s)
+int kinectToOdom(geometry_msgs::PoseStamped &pose,
+					geometry_msgs::Point goalPoint, const char* s)
 { 
-	kinectPose.header.frame_id = s;
-    kinectPose.pose.position.x = x;
-    kinectPose.pose.position.y = y;
-    kinectPose.pose.position.z = z;
-    kinectPose.pose.orientation.x = 1;
-
+	//save goal position in pose
+	pose.header.frame_id = s;
+    pose.pose.position.x = goalPoint.x;
+    pose.pose.position.y = goalPoint.y;
+    pose.pose.position.z = goalPoint.z;
+    pose.pose.orientation.w = 1;
+	
     const string odom = "/odom_combined";
-    listener->transformPose(odom, kinectPose, odomPose);
+    try{
+		//transform pose from s to odom_combined and save it in pose again
+		listener->transformPose(odom, pose, pose);
+	}catch(...){
+		ROS_INFO("ERROR: Transformation failed.");
+		return 0;
+	}
 
-    x = odomPose.pose.position.x;
-    y = odomPose.pose.position.y;
-    z = odomPose.pose.position.z;
-
-    return 0;
+    return 1;
 }
 
 
 void execute(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPtr& goal, Server* as)
 {	
-	suturo_manipulation_msgs::suturo_manipulation_moveResult r;	
-	double x = goal->p.c_centroid.x;
-	double y = goal->p.c_centroid.y;
-	double z = goal->p.c_centroid.z;
 	string arm = goal->arm;
+	suturo_manipulation_msgs::suturo_manipulation_moveResult r;	
 	
-	ROS_INFO("received arm: %s, x: %f, y: %f, z: %f", arm.c_str(), x, y, z);
-	kinectToOdom(x, y, z, goal->p.frame_id.c_str());
-	ROS_INFO("transformed to x: %f, y: %f, z: %f", x, y, z);
-	move_group_interface::MoveGroup group(arm);
-	
-	r.succ.header.stamp;
+	r.succ.header.stamp = ros::Time();
 	r.succ.type = suturo_manipulation_msgs::ActionAnswer::UNDEFINED;
-	ROS_INFO("link: %s", group.getEndEffectorLink().c_str());
-	if (x == 0 && y == 0 && z == 0){
-		group.setNamedTarget(arm+"_home");
-		ROS_INFO("current pos: x=%f, y=%f, z=%f", group.getCurrentPose().pose.position.x,
-				group.getCurrentPose().pose.position.y,
-				group.getCurrentPose().pose.position.z);
-	} else {
-		ROS_INFO("current pos: x=%f, y=%f, z=%f", group.getCurrentPose().pose.position.x,
-				group.getCurrentPose().pose.position.y,
-				group.getCurrentPose().pose.position.z);
-		group.setPositionTarget(x, y, z);	
+	
+	ROS_INFO("received arm: %s, x: %f, y: %f, z: %f", arm.c_str(), goal->p.c_centroid.x,
+		goal->p.c_centroid.y, goal->p.c_centroid.z);
+	
+	//tranform pose
+	geometry_msgs::PoseStamped odomPose;
+	if (!kinectToOdom(odomPose, goal->p.c_centroid, goal->p.frame_id.c_str())){
+		r.succ.type = suturo_manipulation_msgs::ActionAnswer::FAIL;
+		as->setAborted(r);
+		return;
 	}
 	
+	ROS_INFO("transformed to x: %f, y: %f, z: %f", odomPose.pose.position.x,
+		odomPose.pose.position.y, odomPose.pose.position.z);	
+	
+	//set goal Pose
+	move_group_interface::MoveGroup group(arm);
+	
+	group.setPoseTarget(odomPose);
+	ROS_INFO("current Position: x=%f, y=%f, z=%f", group.getCurrentPose().pose.position.x,
+			group.getCurrentPose().pose.position.y,
+			group.getCurrentPose().pose.position.z);
+		
+	//move arm
 	if (group.move()){
 	    r.succ.type = suturo_manipulation_msgs::ActionAnswer::SUCCESS;
 	    as->setSucceeded(r);
@@ -69,8 +74,6 @@ void execute(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPt
 		as->setAborted(r);
 	}	
 	ROS_INFO("moved: %i", r.succ.type);
-	
-
 }
 
 void home(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPtr& goal, Server* as)
@@ -80,10 +83,9 @@ void home(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPtr& 
 	
 	move_group_interface::MoveGroup group(arm);
 	
-	r.succ.header.stamp;
+	r.succ.header.stamp = ros::Time();
 	r.succ.type = suturo_manipulation_msgs::ActionAnswer::UNDEFINED;
 	
-	ROS_INFO("link: %s", group.getEndEffectorLink().c_str());
 	group.setNamedTarget(arm+"_home");
 	ROS_INFO("current pos: x=%f, y=%f, z=%f", group.getCurrentPose().pose.position.x,
 			group.getCurrentPose().pose.position.y,
@@ -102,14 +104,16 @@ void home(const suturo_manipulation_msgs::suturo_manipulation_moveGoalConstPtr& 
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "suturo_manipulation_move_server");
-  ros::NodeHandle n;
-  listener = new (tf::TransformListener);
-  Server server(n, "move_action_server", boost::bind(&execute, _1, &server), false);
-  Server server_home(n, "home_action_server", boost::bind(&home, _1, &server), false);
-  server.start();
-  server_home.start();
-  ROS_INFO("Ready to moveit!.");
-  ros::spin();
-  return 0;
+	ros::init(argc, argv, "suturo_manipulation_move_server");
+	ros::NodeHandle n;
+	listener = new (tf::TransformListener);
+	
+	Server server(n, "move_action_server", boost::bind(&execute, _1, &server), false);
+	Server server_home(n, "home_action_server", boost::bind(&home, _1, &server_home), false);
+	server.start();
+	server_home.start();
+	
+	ROS_INFO("Ready to moveit!.");
+	ros::spin();
+	return 0;
 }
