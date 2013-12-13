99a20318d52e3a0325afdbc79f2d9043629834ff
bennypi <bennypi@fb3.uni-bremen.de>
2013-12-13 15:01:41 +0100

    repository clean-up

--- my_controller_pkg/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/benny/suturo/src/suturo_manipulation/my_controller_pkg/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
