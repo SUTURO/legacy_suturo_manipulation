959c140628a8a56bd7f236c383005a03a365210c
stelter <stelter@tzi.de>
2013-12-15 15:24:00 +0100

    test

--- suturo_manipulation_action/src/head_mover.cpp
+++ suturo_manipulation_action/src/head_mover.cpp
@@ -1,44 +1,8 @@
-/*********************************************************************
-*
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2012, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of Willow Garage, Inc. nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
-
-/* Author: Ioan Sucan */
-
 #include <ros/ros.h>
 
 // MoveIt!
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <suturo_manipulation_gripper_controller.h>
 #include <moveit/move_group_interface/move_group.h>
 #include <shape_tools/solid_primitive_dims.h>
 
@@ -72,7 +36,7 @@ void pick(moveit::planning_interface::MoveGroup &group)
 
   g.pre_grasp_posture.name.resize(1, "r_gripper_l_finger_joint");
   g.pre_grasp_posture.position.resize(1);
-  g.pre_grasp_posture.position[0] = 0.545;
+  g.pre_grasp_posture.position[0] = 0.55;
   g.grasp_posture.name.resize(1, "r_gripper_l_finger_joint");
   g.grasp_posture.position.resize(1);
   g.grasp_posture.position[0] = 0.3;
@@ -265,10 +229,10 @@ int main(int argc, char **argv)
   co.operation = moveit_msgs::CollisionObject::ADD;
   co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X] = 0.5;
   co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 1.5;
-  co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.35;
+  co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.69;
   co.primitive_poses[0].position.x = 0.7;
   co.primitive_poses[0].position.y = -0.2;
-  co.primitive_poses[0].position.z = 0.175;
+  co.primitive_poses[0].position.z = 0;
   pub_co.publish(co);
 
 
@@ -292,13 +256,13 @@ int main(int argc, char **argv)
   // wait a bit for ros things to initialize
   ros::WallDuration(1.0).sleep();
 
-openhand();
+//openhand();
 
 ROS_INFO("pick");
 
   //group.pick("part");
 
-  //pick(group);
+  pick(group);
 
   ros::WallDuration(5.0).sleep();
   ROS_INFO("PLACE");
