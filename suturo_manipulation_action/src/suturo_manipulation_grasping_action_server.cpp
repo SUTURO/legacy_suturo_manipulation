58d4ccac13ad2ca283068780e0ac50c69183cf74
thocar <thocar@tzi.de>
2014-01-11 12:16:59 +0100

    MAN-34 finished action server, had to be tested if pick and drop is implemented

--- suturo_manipulation_action/src/suturo_manipulation_grasping_action_server.cpp
+++ suturo_manipulation_action/src/suturo_manipulation_grasping_action_server.cpp
@@ -9,18 +9,58 @@ using namespace std;
 
 typedef actionlib::SimpleActionServer<suturo_manipulation_msgs::suturo_manipulation_graspingAction> Server_grasp;
 
-void grasp(const suturo_manipulation_msgs::suturo_manipulation_graspingGoalConstPtr& goal, ros::NodeHandle* nh, Server_grasp* as)
+/**
+* This method grasps or drops a object!
+*/
+void grop(const suturo_manipulation_msgs::suturo_manipulation_graspingGoalConstPtr& graspGoal, ros::NodeHandle* nh, Server_grasp* as)
 {	
 	suturo_manipulation_msgs::suturo_manipulation_graspingResult r;	
 	
 	r.succ.header.stamp = ros::Time();
 	r.succ.type = suturo_manipulation_msgs::ActionAnswer::UNDEFINED;
 
-	//0 für fail
-
 	// set Planning Interface and Grasper
+	ROS_INFO("Create Planning Scene Interface...");
 	Suturo_Manipulation_Planning_Scene_Interface pi(nh);
+	ROS_INFO("Done. Create Grasper...")	;
 	Grasping grasper(&pi);
+	ROS_INFO("Done.");
+
+	// Set arm to pick and object name
+	string picking_arm = graspGoal->goal.arm;
+	ROS_INFO("Arm to pick: %s", picking_arm.c_str());
+	string obj_name = graspGoal->goal.objectName;
+	ROS_INFO("ObjectName to pick: %s", obj_name.c_str());
+
+	// Check if we should grasp or drop...
+	if(graspGoal->goal.grasp){
+		ROS_INFO("Begin to pick object...");
+		// Grasp the object
+		if (grasper.pick(obj_name, picking_arm))
+		{
+			ROS_INFO("Object picked...");
+			r.succ.type = suturo_manipulation_msgs::ActionAnswer::SUCCESS;
+	    as->setSucceeded(r);
+		} else {
+			ROS_INFO("Picking failed!");
+			r.succ.type = suturo_manipulation_msgs::ActionAnswer::FAIL;
+		as->setAborted(r);
+		}
+	} else {
+		ROS_INFO("Begin to drop object...");
+		// Drop the object
+		if (grasper.drop(obj_name))
+		{
+			ROS_INFO("Object droped...");
+			r.succ.type = suturo_manipulation_msgs::ActionAnswer::SUCCESS;
+	    as->setSucceeded(r);
+		} else {
+			ROS_INFO("Droping failed!");
+			r.succ.type = suturo_manipulation_msgs::ActionAnswer::FAIL;
+		as->setAborted(r);
+		}
+	}
+
 
 }
 
@@ -30,7 +70,7 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "suturo_manipulation_grasp_server");
 	ros::NodeHandle nh;
 	
-	Server_grasp server(nh, "suturo_man_grasping_server", boost::bind(&grasp, _1, &nh, &server), false);
+	Server_grasp server(nh, "suturo_man_grasping_server", boost::bind(&grop, _1, &nh, &server), false);
 	server.start();
 	
 	ROS_INFO("Ready to move the head!");
