be502f1779bb5c65305502aea7e105073d42c575
stelter <stelter@tzi.de>
2014-03-29 16:33:52 +0100

    modified gazebo launch files

--- suturo_manipulation_gazebo/launch_simulation.bash
+++ suturo_manipulation_gazebo/launch_simulation.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-. ~/.bashrc
+source $(rospack find suturo_manipulation_gazebo)/../../../devel/setup.bash
 export GAZEBO_MODEL_PATH=$(rospack find suturo_manipulation_gazebo)/models
 roslaunch suturo_manipulation_gazebo gazebo_pr2_test_simulation.launch
+source ~/.bashrc
 
--- suturo_manipulation_gazebo/launch_simulation.zsh
+++ suturo_manipulation_gazebo/launch_simulation.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-source ~/.zshrc
+source $(rospack find suturo_manipulation_gazebo)/../../../devel/setup.zsh
 export GAZEBO_MODEL_PATH=$(rospack find suturo_manipulation_gazebo)/models
 roslaunch suturo_manipulation_gazebo gazebo_pr2_test_simulation.launch
-
+source ~/.zshrc
