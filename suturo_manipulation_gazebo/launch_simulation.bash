6f90905fe993a5477497ebc4963fc4849223c873
Moritz Horstmann <mhorst@tzi.de>
2014-04-04 00:32:24 +0200

    Fix handling with symlinked suturo directories.

--- suturo_manipulation_gazebo/launch_simulation.bash
+++ suturo_manipulation_gazebo/launch_simulation.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-source $(rospack find suturo_manipulation_gazebo)/../../../devel/setup.bash
+GROOVY_PATH=$(rospack find suturo_manipulation_gazebo)/../../../devel
+source $(cd $GROOVY_PATH && pwd)/setup.bash
 export GAZEBO_MODEL_PATH=$(rospack find suturo_manipulation_gazebo)/models
 roslaunch suturo_manipulation_gazebo gazebo_pr2_test_simulation.launch
 source ~/.bashrc
--- suturo_manipulation_gazebo/launch_simulation_with_3_obj.zsh
+++ suturo_manipulation_gazebo/launch_simulation_with_3_obj.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-source $(rospack find suturo_manipulation_gazebo)/../../../devel/setup.zsh
+GROOVY_PATH=$(rospack find suturo_manipulation_gazebo)/../../../devel
+source $(cd $GROOVY_PATH && pwd)/setup.bash
 export GAZEBO_MODEL_PATH=$(rospack find suturo_manipulation_gazebo)/models
 roslaunch suturo_manipulation_gazebo gazebo_pr2_test_simulation_with_3_obj.launch
 source ~/.zshrc
