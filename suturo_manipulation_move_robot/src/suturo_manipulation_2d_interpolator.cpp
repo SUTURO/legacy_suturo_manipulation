9314e9385c50993f89549c84c87db5896522ae71
thocar <thocar@tzi.de>
2014-05-09 19:10:58 +0200

    MAN-74 more docu

--- suturo_manipulation_move_robot/src/suturo_manipulation_2d_interpolator.cpp
+++ suturo_manipulation_move_robot/src/suturo_manipulation_2d_interpolator.cpp
@@ -1,5 +1,10 @@
 #include "suturo_manipulation_2d_interpolator.h"
 
+/**
+* Checks if the given target is already in range of the robot.
+* @return True if the robot is in rage to the target,
+*           False if not
+*/
 bool targetInRange(const pose_2d &robot_pose, const pose_2d &target_pose, double range)
 {
     double x_diff = robot_pose.x_ - target_pose.x_;
@@ -18,6 +23,7 @@ void Suturo_Manipulation_2d_Interpolator::init(const interpolator_2d_init_params
     trajectory_input_ = new RMLPositionInputParameters(dof_);
     trajectory_output_ = new RMLPositionOutputParameters(dof_);
 
+    // fill trajectory input with basic data
     for (int i = 0; i < dof_; ++i)
     {
         trajectory_input_->SetMaxVelocityVectorElement(params.vel_limit_, i);
@@ -37,11 +43,14 @@ const interpolator_2d_result &Suturo_Manipulation_2d_Interpolator::interpolate(c
     struct pose_2d int_pose;
     struct interpolator_2d_result *ir = new interpolator_2d_result();
 
+    // Check reference frame...
     if (params.robot_pose_.reference_ != params.target_pose_.reference_)
     {
         ROS_ERROR("robot_pose_.reference_ != target_pose_.reference_!! Frame must be equal!!");
     }
 
+    // Set Input to interpolate. If the target is already in the given range, 
+    // the target is equivalent to the robot position
     if (targetInRange(params.robot_pose_, params.target_pose_, radius_range_))
     {
         trajectory_input_->SetCurrentPositionVectorElement(params.robot_pose_.x_, 0);
@@ -71,6 +80,7 @@ const interpolator_2d_result &Suturo_Manipulation_2d_Interpolator::interpolate(c
         ROS_WARN("Trajectory input was not valid.");
     }
 
+    // interpolate...
     rv_ = trajectory_generator_->RMLPosition(*trajectory_input_, trajectory_output_, trajectory_generator_flags_);
 
 
@@ -79,9 +89,11 @@ const interpolator_2d_result &Suturo_Manipulation_2d_Interpolator::interpolate(c
         ROS_ERROR("Trajectory generator returned with an error.");
     }
 
+    // Save the interpolated pose in int_pose
     trajectory_output_->GetNewPositionVectorElement(&int_pose.x_, 0);
     trajectory_output_->GetNewPositionVectorElement(&int_pose.y_, 1);
 
+    // Save the calculated twist in ip_twist
     trajectory_output_->GetNewVelocityVectorElement(&ip_twist.xdot_, 0);
     trajectory_output_->GetNewVelocityVectorElement(&ip_twist.ydot_, 1);
 
